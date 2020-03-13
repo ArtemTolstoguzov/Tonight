5431adb7400df258d969ae502937f6191c03aa0e
DipCoy <dipolarcoyote@gmail.com>
2020-03-13 13:55:23 +0500

    Preparation

---
+++ Tonight/Program.cs
@@ -0,0 +1,35 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tonight
+{
+    class Program
+    {
+        static RenderWindow win;
+
+        static void Main(string[] args)
+        {
+            win = new RenderWindow(new SFML.Window.VideoMode(800, 600), "Tonight");
+            win.SetVerticalSyncEnabled(true);
+
+            win.Closed += WinClosed;
+            while (win.IsOpen)
+            {
+                win.DispatchEvents();
+
+                win.Clear(Color.Black);
+
+                win.Display();
+            }
+        }
+
+        private static void WinClosed(object sender, EventArgs e)
+        {
+            win.Close();
+        }
+    }
+}
